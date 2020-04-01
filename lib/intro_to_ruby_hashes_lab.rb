0d757689aca821bbb43dda7cda715264f5ac1096
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-04-01 18:49:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ pioneer[:name]
 
 def id_hash_generator(number)
   id_hash_generator = {
-    :id => id
+    :id => number
   }  
   # return a hash with a key :id assigned to the provided number
 end
